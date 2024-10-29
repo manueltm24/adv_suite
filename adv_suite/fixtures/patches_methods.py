aec9ff891f42377096b0d594000796a9e776dcd0
jcruzram <jcruzram@gmail.com>
2024-10-29 02:30:59 -0600

    Fix custom field

--- adv_suite/fixtures/patches_methods.py
+++ adv_suite/fixtures/patches_methods.py
@@ -5,6 +5,8 @@ def remove_custom_fields_project():
 
     if frappe.db.exists("Custom Field", "Project-custom_required_margin"):
         frappe.delete_doc("Custom Field", "Project-custom_required_margin")
+        frappe.clear_cache()
+        frappe.db.commit() 
         print("Campo 'custom_required_margin' eliminado.")
 
     print("Se eliminaron los custom fields obsoletos de los Proyectos.")
