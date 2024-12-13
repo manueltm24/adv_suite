9a4c7f0af835c0e1f91fb238ce0907c6029ee755
jcruzram <jcruzram@gmail.com>
2024-12-12 18:59:33 -0600

    Material devuelto. WO

--- adv_suite/overrides.py
+++ adv_suite/overrides.py
@@ -31,3 +31,32 @@ class CustomWorkOrder(WorkOrder):
             row.db_set(
                 "transferred_qty", (transferred_items.get(row.item_code) or 0.0), update_modified=False
             )
+
+    def update_returned_qty(self):
+        ste = frappe.qb.DocType("Stock Entry")
+        ste_child = frappe.qb.DocType("Stock Entry Detail")
+
+        query = (
+            frappe.qb.from_(ste)
+            .inner_join(ste_child)
+            .on(ste_child.parent == ste.name)
+            .select(
+                ste_child.item_code,
+                ste_child.original_item,
+                fn.Sum(ste_child.qty).as_("qty"),
+            )
+            .where(
+                (ste.docstatus == 1)
+                & (ste.work_order == self.name)
+                & ((ste.purpose == "Material Transfer for Manufacture") | (ste.purpose == "Material Transfer"))
+                & (ste.is_return == 1)
+            )
+            .groupby(ste_child.item_code)
+        )
+
+        data = query.run(as_dict=1) or []
+        returned_dict = frappe._dict({d.original_item or d.item_code: d.qty for d in data})
+
+        for row in self.required_items:
+            row.db_set("returned_qty", (returned_dict.get(row.item_code) or 0.0), update_modified=False)
+
